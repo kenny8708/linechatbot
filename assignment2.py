c611cda9d0336d47998e705513d120d79430ddcd
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-02-29 23:48:05 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -81,7 +81,7 @@ def callback():
 # Handler function for Text Message
 def handle_TextMessage(event):
     print(event.message.text)
-    msg = 'You said: "' + event.message.text + '" '
+    msg = 'You said: !!"' + event.message.text + '" '
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(msg)
