e9a49863ab8f1c4fd3d9f21028425c4fe6602e15
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-03-14 23:53:02 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -115,7 +115,7 @@ def line_select_overall(text):
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cursor = conn.cursor()
-    postgres_select_query = f"""SELECT response FROM Response WHERE keyword = 'mask';"""
+    postgres_select_query = f"""SELECT response FROM Response WHERE keyword = '%s';"""
     cursor.execute(postgres_select_query)
     record = cursor.fetchall()
 
