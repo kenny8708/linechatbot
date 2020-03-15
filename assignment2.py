8a1aeb0f881c77f9dc4774574fe6de9133d28a61
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-03-16 00:18:09 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -144,7 +144,11 @@ def handle_TextMessage(event):
          )
     else:
          X = redis1.incr(event.message.text)
-         print('for',X,'times') 
+         #print('for',X,'times') 
+         line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=X)
+        )
          msg = 'I don\'t understand "' + event.message.text + '"'
          line_bot_api.reply_message(
             event.reply_token,
