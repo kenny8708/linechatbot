77238066f43787d6df37e91d9267086e0fb4d906
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-03-15 21:26:56 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -135,8 +135,8 @@ def handle_TextMessage(event):
             TextSendMessage(text='Please retry it later')
          )
     else:
-        while True:
-                msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
+        msg = 'I don\'t understand "' + event.message.text + '" '
+        msg = input("Please enter your query (type 'quit' or 'exit' to end):").strip()
             if msg == 'quit' or msg == 'exit':
                 break
             if msg == '':
@@ -144,8 +144,7 @@ def handle_TextMessage(event):
             msg = "You have entered " + msg, end=' '"
             X = redis1.incr(msg)
             print('for',X,'times')    
-         msg = 'I don\'t understand "' + event.message.text + '" '
-         line_bot_api.reply_message(
+        line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=msg)
         )
