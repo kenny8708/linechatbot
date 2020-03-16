6d2a1ab8dae7501517243adcb3645a212ba72815
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-03-16 21:58:04 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -120,7 +120,7 @@ def handle_TextMessage(event):
          )
     elif 'Case'  in event.message.text:
         try:
-         response=exec(open("hk_covid19.py").read())
+         repsonse=exec(open("hk_covid19.py").read())
          line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(repsonse)
