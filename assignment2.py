f95225606bb875448183e88e0f556a66a706789a
Kwok Man Hin <19411081@life.hkbu.edu.hk>
2020-03-16 17:14:05 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -5,6 +5,9 @@ import sys
 import redis
 import psycopg2
 import datetime
+import pandas as pd  
+import io  
+import requests
 
 from argparse import ArgumentParser
 
@@ -46,6 +49,18 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
+# Latest situation of reported cases of COVID-19 in Hong Kong
+url1="http://www.chp.gov.hk/files/misc/latest_situation_of_reported_cases_wuhan_eng.csv"  
+s=requests.get(url1).content  
+hk=pd.read_csv(io.StringIO(s.decode('utf-8')))
+hk1=hk.iloc[-1]['As of date']
+hk2=hk.iloc[-1]['Number of confirmed cases']
+hk3=hk.iloc[-1]['Number of ruled out cases']
+hk4=hk.iloc[-1]['Number of cases still hospitalised for investigation']
+hk5=hk.iloc[-1]['Number of cases fulfilling the reporting criteria']
+hk6=hk.iloc[-1]['Number of death cases']
+hk7=hk.iloc[-1]['Number of discharge cases']
+hk8=hk.iloc[-1]['Number of probable cases']
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -122,7 +137,18 @@ def handle_TextMessage(event):
          repsonse = line_select_overall(event.message.text)
          line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=repsonse)
+            TextSendMessage(text='Latest COVID-19 Statistics in HK', "\n"
+      'Confirmed:', hk2, "\n"
+      'Probable:', hk3, "\n"
+      'Death:', hk4, "\n"
+      'Discharged:', hk5, "\n"
+      'Hospitalised:', hk6, "\n"
+      'Ruled out:', hk7, "\n"
+      'Reported:', hk8, "\n"
+      '---------', "\n"
+      'Data Source: data.gov.hk', "\n"
+      'Last Updated on:', hk1, "\n"
+      'Update Frequency: Every Night'
          )
         except:
          line_bot_api.reply_message(
