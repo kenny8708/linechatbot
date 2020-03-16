a6c06db482e8c2fe1de16eb0f428027fd112916f
Kwok Man Hin <19411081@life.hkbu.edu.hk>
2020-03-17 01:56:38 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -115,7 +115,7 @@ hk8=hk.iloc[-1]['Number of probable cases']
 def handle_TextMessage(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=f'Latest COVID-19 Statistics in HK \n Confirmed: {hk2} Probable: {hk3} Death: {hk4} Discharged: {hk5} Hospitalised: {hk6} Ruled out: {hk7} Reported: {hk8}'
+        TextSendMessage(text=f'Latest COVID-19 Statistics in HK \nConfirmed: {hk2} \nProbable: {hk3} \nDeath: {hk4} \nDischarged: {hk5} \nHospitalised: {hk6} \nRuled out: {hk7} \nReported: {hk8}'
         )
     )
 
