30121d255cd213edea946e99423429054c02a2dd
kenny8708 <40491544+kenny8708@users.noreply.github.com>
2020-03-17 23:23:41 +0800

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -115,8 +115,8 @@ def handle_TextMessage(event):
 #  Text Message (Latest COVID-19 Statistics in HK)
     if event.message.text == "HK Stat": 
         try:   
-        line_bot_api.reply_message(
-            event.reply_token,
+         line_bot_api.reply_message(
+           event.reply_token,
             TextSendMessage(text=f'Latest COVID-19 Statistics in HK \nConfirmed: {hk2} \nProbable: {hk3} \nDeath: {hk4} \nDischarged: {hk5} \nHospitalised: {hk6} \nRuled out: {hk7} \nReported: {hk8} \n--------- \nData Source: data.gov.hk \nLast Updated on: {hk1} \nUpdate Frequency: Every Night' 
             )
         )
@@ -139,9 +139,9 @@ def handle_TextMessage(event):
 # Buttons Template (Latest COVID-19 Statistics in HK)
     if event.message.text == "Case":
         try: 
-        buttons_template = TemplateSendMessage(
-        alt_text='Please check on smartphone.',
-        template=ButtonsTemplate(
+         buttons_template = TemplateSendMessage(
+         alt_text='Please check on smartphone.',
+         template=ButtonsTemplate(
             title='Hong Kong',
             text='Please select',
             thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Flag_of_Hong_Kong.svg/2560px-Flag_of_Hong_Kong.svg.png',
@@ -155,9 +155,9 @@ def handle_TextMessage(event):
                     uri='https://chp-dashboard.geodata.gov.hk/covid-19/en.html'
                 ),
             ]
-        )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
+         )
+         )
+         line_bot_api.reply_message(event.reply_token, buttons_template)
         except:
          line_bot_api.reply_message(
            event.reply_token,
@@ -209,9 +209,9 @@ def handle_TextMessage(event):
                 ]
             )
         ]
-    )
-    )
-    line_bot_api.reply_message(event.reply_token,carousel)
+     )
+     )
+     line_bot_api.reply_message(event.reply_token,carousel)
        except:
          line_bot_api.reply_message(
            event.reply_token,
@@ -220,7 +220,7 @@ def handle_TextMessage(event):
 #Location function
     if event.message.text == "Mask Location":
         try:    
-        line_bot_api.reply_message(event.reply_token,LocationSendMessage(
+         line_bot_api.reply_message(event.reply_token,LocationSendMessage(
             title='Mask location', 
             address='Kai Tin Shopping Centre', 
             latitude=22.308132, 
