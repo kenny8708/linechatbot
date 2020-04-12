1dcc42af42fc8270c929bdc3549e2e4e9035de40
Kwok Man Hin <19411081@life.hkbu.edu.hk>
2020-04-13 02:13:57 +0800

    Update project.py

--- project.py
+++ project.py
@@ -249,11 +249,11 @@ def handle_TextMessage(event):
                  actions=[
                     URIAction(
                         label='Hong Kong Island',
-                        uri='https://www.ha.org.hk/visitor/ha_isf_result.asp?lang=ENG&service_code_id=461&hospital_type=&service_type=GOPD&location=HK'
+                        uri='https://www.ha.org.hk/haho/ho/cc-Wuhan/GOPC_service_arrangement_for_appointment_change_and_medication_refill_en_text.pdf'
                     ),
                     URIAction(
-                        label='Kowloon',
-                        uri='https://www.ha.org.hk/visitor/ha_isf_result.asp?lang=ENG&service_code_id=461&hospital_type=&service_type=GOPD&location=KLN'
+                        label='Non-office Hours',
+                        uri='https://www.ha.org.hk/haho/ho/hesd/Public_Holiday_2020_eng_txt.pdf'
                     ),
                     URIAction(
                         label='New Territories',
