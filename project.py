6623e5ef00074723f9cdaba0886085b6f799d237
unknown <kenny870812@gmail.com>
2020-04-14 00:52:28 +0800

    Update project.py

--- project.py
+++ project.py
@@ -368,7 +368,7 @@ def handle_TextMessage(event):
             TextSendMessage(text='Please retry it later')
          )
 
-    if translator.translate(event.message.text).text == "About":
+    if translator.translate(event.message.text).text == "About Us":
         try:    
          Image_Carousel = TemplateSendMessage(
             alt_text='目錄 template',
