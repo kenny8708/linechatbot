6fc041dffe2e718daae8d705b46b2b6bb0be1ecf
Kwok Man Hin <19411081@life.hkbu.edu.hk>
2020-04-13 17:35:42 +0800

    Update project.py

--- project.py
+++ project.py
@@ -153,7 +153,7 @@ def handle_TextMessage(event):
         try:   
          line_bot_api.reply_message(
            event.reply_token,
-            TextSendMessage(text=f'Case {cc_number0} {cc_number6} \n\n{cc_number7} \nGender: {cc_number3} \nAge: {cc_number4} \n{cc_number8} \n{cc_number1} {cc_number9} \n{cc_number2} Onset \n\nHospital admitted:\n{cc_number5} \n{blist_name2} \n{blist_name1}'
+            TextSendMessage(text=f'Case {cc_number0} {cc_number6} \n\n{cc_number7} \nGender: {cc_number3} \nAge: {cc_number4} \n{cc_number8} \n{cc_number1} {cc_number9} \n{cc_number2} Onset \n\nHospital admitted:\n{cc_number5} \n\nBuildings in which cases have resided:\n{blist_name2} \n{blist_name1}'
             )
         )
         except:
